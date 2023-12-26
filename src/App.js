ecca8aadce896c185e1ddd2d8f22f613fd433ef1
ajaykumar0097 <omkashyap9536@gmail.com>
2023-12-26 23:11:22 +0530

    add button functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Header } from "./components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <Header />/
+      <Header />
+    
       <Body/>
     </div>
   );
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,21 +1,56 @@
+import { useState } from "react";
 import { KFC_OBJ } from "../../utils/__mock__/__mock__";
 import ResCard from "./ResCard/ResCard";
 
-const Body =() => {
-    return(
-      <div className="body">
-        <div className="search">
-            <button className="filter-btn"  onClick={() => console.log("clicked")}>Top Rated Restaurants</button>
-        </div>
-        <div className="res-container">
-        
-          {
-              KFC_OBJ.map((item)=> <ResCard resData={item}/>)
-          }
-         
-        </div>
-      </div>
-    )
-  }
+// const listOfRestraurantJs =
+//     [
+//       {
+//       info: {
+//         name: "ajay",
+//         avgRating: "3"
 
-  export default Body;
+//       }
+//     },
+//     {
+//       info: {
+//         name: "kmr",
+//         avgRating: "4.5"
+
+//       }
+//     },
+//     {
+//       info: {
+//         name: "kas",
+//         avgRating: "5"
+
+//       }
+//     }
+//   ]
+const Body = () => {
+  const [listOfRestraurant, setListOfRestraurant]= useState(KFC_OBJ)
+  
+
+  const handleClick=() => {
+    const listOfRestraurant1=listOfRestraurant.filter((res) => res.info.avgRating>4.2
+
+    
+    )
+    setListOfRestraurant(listOfRestraurant1);
+  }
+  return (
+    <div className="body">
+      <div className="search">
+        <button className="filter-btn" onClick={handleClick}>Top Rated Restaurants</button>
+      </div>
+      <div className="res-container">
+
+        {
+          listOfRestraurant.map((item) => <ResCard resData={item} />)
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default Body;
